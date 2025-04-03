2ffb97af75549de0cb18aa49138105033062d334
RahwaZeslusHaile <rahwazeslase@gmail.com>
2025-04-03 09:58:06 +0100

    View an individual article is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import Header from "./component/Header";
 import Nav from "./component/Nav";
 import Home from "./component/Home";
-import ArticleList from "./component/ListContainer/ArticlesList"
+import ArticleList from "./component/ListContainer/ArticlesList";
 import AddArticle from "./component/ListContainer/Adder";
 import FilteredArticles from "./component/ListContainer/Filter";
 import UserProvider from "./Context/UserContext";
 import Footer from "./component/Footer";
 import { Routes, Route } from "react-router-dom";
-import "./App.css"; 
+import "./App.css";
 
 function App() {
   return (
-    <UserProvider> 
+    <UserProvider>
       <div id="app">
         <Header />
         <Nav />
@@ -21,11 +21,12 @@ function App() {
             <Route path="/add" element={<AddArticle />} />
             <Route path="/filter" element={<FilteredArticles />} />
             <Route path="/articles" element={<ArticleList />} />
+            <Route path="/articles/:article_id" element={<ArticleList />} />
           </Routes>
         </main>
         <Footer />
       </div>
-    </UserProvider>  
+    </UserProvider>
   );
 }
 
--- src/component/ListContainer/ArticlesList.jsx
+++ src/component/ListContainer/ArticlesList.jsx
@@ -1,25 +1,41 @@
 import { useState, useEffect } from "react";
-import fetchArticles from "../../api"; 
+import { fetchArticles, fetchArticlesByArticleId } from "../../api"; 
 import ArticleCard from "./ArticleCard";
+import { useParams } from 'react-router-dom';
 
 function ArticleList() {
   const [articles, setArticles] = useState([]);
+  const [selectedArticleId, setSelectedArticleId] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
+  const { article_id } = useParams(); 
 
   useEffect(() => {
     setError(null);
     setIsLoading(true);
-    fetchArticles()
-      .then((data) => {
-        setArticles(data.article);
-        setIsLoading(false);
-      })
-      .catch(() => {
-        setError("Error fetching data");
-        setIsLoading(false);
-      });
-  }, []);
+
+    if (article_id) {
+      fetchArticlesByArticleId(article_id)
+        .then((data) => {
+          setArticles([data.article]);
+          setIsLoading(false);
+        })
+        .catch(() => {
+          setError("Error fetching article");
+          setIsLoading(false);
+        });
+    } else {
+      fetchArticles()
+        .then((data) => {
+          setArticles(data.article); 
+          setIsLoading(false);
+        })
+        .catch(() => {
+          setError("Error fetching data");
+          setIsLoading(false);
+        });
+    }
+  }, [article_id]); 
 
   if (isLoading) return <p>Loading Articles...</p>;
   if (error) return <p>{error}</p>;
@@ -27,7 +43,13 @@ function ArticleList() {
   return (
     <div className="article-wrapper">
       {articles.map((article) => (
-        <ArticleCard key={article.article_id} {...article} />
+        <ArticleCard
+          key={article.article_id}
+          article={article}
+          isSingleArticleView={Boolean(article_id)}
+          isSelected={selectedArticleId === article.article_id}
+          onSelect={() => setSelectedArticleId(article.article_id)}
+        />
       ))}
     </div>
   );
